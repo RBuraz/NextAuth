a72c3ab7df1aae20a4e481fb3449c46c673d8820
RBuraz <rburaz00@outlook.com>
2024-05-08 08:14:40 +0200

    first commit 1:22:58

---
+++ components/auth/card-wrapper.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+import Header from "@/components/auth/header";
+import Social from "@/components/auth/social";
+import BackButton from "./back-button";
+
+interface CardWrapperProps {
+  children: React.ReactNode;
+  headerLable: string;
+  backButtonLabel: string;
+  backButtonHref: string;
+  showSocial?: Boolean;
+}
+
+export const CardWrapper = ({
+  children,
+  headerLable,
+  backButtonLabel,
+  backButtonHref,
+  showSocial,
+}: CardWrapperProps) => {
+  return (
+    <Card className="w-[400px] shadow-md">
+      <CardHeader>
+        <Header label={headerLable} />
+      </CardHeader>
+      <CardContent>{children}</CardContent>
+      {showSocial && (
+        <CardFooter>
+          <Social />
+        </CardFooter>
+      )}
+      <CardFooter>
+        <BackButton label={backButtonLabel} href={backButtonHref} />
+      </CardFooter>
+    </Card>
+  );
+};
